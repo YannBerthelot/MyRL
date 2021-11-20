a2390898d44bf4c93a3be4ee5ed5d8b4914d1f4b
YANN BERTHELOT <yann.berthelot@capgemini.com>
2021-11-20 13:01:28 +0100

    update unit test to add SARSA

--- test_agent.py
+++ test_agent.py
@@ -3,19 +3,21 @@ import unittest
 import gym
 import logging
 import sys
+from app.agents.sarsa import SARSA
 from app.agents.qlearning import Q_learning
-from app import logger
+from app import logger as lg
+from parameterized import parameterized
 
-logger.level = logging.DEBUG
+lg.level = logging.DEBUG
 stream_handler = logging.StreamHandler(sys.stdout)
-logger.addHandler(stream_handler)
+lg.addHandler(stream_handler)
 
-env = gym.make("FrozenLake-v0", is_slippery=True)
+env = gym.make("FrozenLake-v1", is_slippery=True)
 
 
-class TestQLearning(unittest.TestCase):
+class TestAgent(unittest.TestCase):
     def test_init_q_table(self):
-        Q_Learner = Q_learning(env)
+        Q_Learner = SARSA(env)
         # case 0
         self.assertEqual(
             Q_Learner.init_q_table().shape,
@@ -31,29 +33,27 @@ class TestQLearning(unittest.TestCase):
             {1},
         )
 
-    def test_train(self):
-        Q_Learner = Q_learning(env)
-        Q_Learner.train()
-
     def test_test(self):
-        Q_Learner = Q_learning(env)
+        Q_Learner = SARSA(env)
         with self.assertRaises(ValueError):
             Q_Learner.test()
         Q_Learner.train()
         Q_Learner.test()
 
-    def test_full_train_test(self):
+    @parameterized.expand([[SARSA], [Q_learning]])
+    def test_full_train_test(self, agent_class):
         # No slip
-        env = gym.make("FrozenLake-v0", is_slippery=False)
-        Q_Learner = Q_learning(env)
+        env = gym.make("FrozenLake-v1", is_slippery=False)
+        lg.info(f"Testing {agent_class} on environment {env}")
+        Q_Learner = SARSA(env)
         params = {"learning_rate": 0.1, "epsilon": 0.05, "discount factor": 0.999}
         Q_Learner.train(n_episode=1000, params=params, verbose=False)
         self.assertEqual(
             Q_Learner.test(n_episode=1, verbose=False)["success_rate"], 100.0
         )
         # Slippery
-        env = gym.make("FrozenLake-v0", is_slippery=True)
-        Q_Learner = Q_learning(env)
+        env = gym.make("FrozenLake-v1", is_slippery=True)
+        Q_Learner = SARSA(env)
         params = {"learning_rate": 0.1, "epsilon": 0.05, "discount factor": 0.999}
         Q_Learner.train(n_episode=20000, params=params, verbose=False)
         self.assertGreaterEqual(
